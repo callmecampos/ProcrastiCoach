73c083d5e803f4aaa63b63b3b10a1b34df65d716
Felipe Rocha Campos <fmrcampos@berkeley.edu>
2018-06-12 03:19:50 -0700

    initial commit, making extension prototype

---
+++ popup.js
@@ -0,0 +1,9 @@
+function hello() {
+  prompt("Enter something!", "Text");
+}
+
+// check if switch is on
+// if so, check if on listed website
+// if so, show popup asking user their purpose for visiting the website
+// periodically remind user in off-to-side pop-up
+// provide support and have work-mode, fun-mode, movie-mode, etc.
